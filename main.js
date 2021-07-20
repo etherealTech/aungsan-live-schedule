84fb3e85fdd9382526567f457cbb977d68ba934d
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-21 00:14:27 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+const { default: axios } = require('axios');
+const getFBVideo = require('./src/getFBVideo');
+
+const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
+const postID = '654432202109918';
+
 !async function () {
   console.log(new Date().toLocaleString('en-US', {
     timeZone: 'Asia/Yangon'
   }, '[STARTED]'));
-  console.log(...process.argv.slice(2));
+  
+  const videoSources = await getFBVideo(postID);
+  
+  console.log(videoSources):
+  
+  axios.get(`https://graph.facebook.com/v10.0/me?access_token=${FACEBOOK_PAGE_TOKEN}`)
+    .then(({ data }) => {
+      console.log(data);
+    });
 }();
