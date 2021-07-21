f481567027f51861013e4a8406151ca11a48d76d
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-22 02:00:32 +0630

    Update generate-readme.js

--- generate-readme.js
+++ generate-readme.js
@@ -6,7 +6,7 @@ const CRON_SCHEDULE_TIME = process.argv[3];
 const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
 
 !async function() {
-  const { data } = await axios.get(`https://graph.facebook.com/v10.0/me?${FACEBOOK_PAGE_TOKEN}`);
+  const { data } = await axios.get(`https://graph.facebook.com/v10.0/me?access_token=${FACEBOOK_PAGE_TOKEN}`);
   generateReadme(data, CRON_SCHEDULE_TIME);
   pushChanges();
 }();
