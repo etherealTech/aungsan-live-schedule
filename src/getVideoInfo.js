d5dcf737320d6b6cc7764a609b97c79bfaf08993
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-07-21 17:44:31 +0630

    Update getVideoInfo.js

--- src/getVideoInfo.js
+++ src/getVideoInfo.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
 const { readFileSync, writeFileSync } = require('fs');
+const { exec } = require('shelljs');
 
 const VIDEO_DATA = resolve(__dirname, '../data/videos.json');
 const DATABASE_PATH = resolve(__dirname, '../.db');
@@ -42,9 +43,24 @@ function updateVideo() {
   };
   data.index = video.index + 1;
   data.logs.push(log);
+  writeFileSync(DATABASE_PATH, JSON.stringify(data), "utf-8");
+}
+
+function pushChanges(message) {
+  let command = [
+    "git config user.name 'skmetheloper'",
+    "git config user.email 'ethereal97@gmail.com'",
+    "git add .db",
+    "git commit -m 'Live Schedule'",
+    "git push"
+  ].join(";\n");
+
+  exec(command);
+  console.log('[GIT:PUSH]');
 }
 
 module.exports = {
   getVideo,
   updateVideo,
+  pushChanges,
 };
