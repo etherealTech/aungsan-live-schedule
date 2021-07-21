7eb371a36e385799fd89a40499a821727f5ad0d8
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-22 05:41:40 +0630

    Update createLiveStream.js

--- src/createLiveStream.js
+++ src/createLiveStream.js
@@ -1,18 +1,16 @@
 const { default: axios } = require('axios');
 const FACEBOOK_GRAPH_URL = 'https://graph.facebook.com';
 
-module.exports = async function createLiveStream({ 
+module.exports = function createLiveStream({ 
   title, 
   description,
   access_token
 }) { 
   let url = `${FACEBOOK_GRAPH_URL}/me/live_videos`;
-  let { data } = await axios.post(url, {
+  return axios.post(url, {
     access_token,
     status: 'LIVE_NOW',
     title,
     description,
-    fields: 'id,stream_url,video',
-  });
-  return data;
+  }).then(({ data }) => data);
 };
