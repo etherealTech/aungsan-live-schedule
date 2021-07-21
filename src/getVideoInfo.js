1a23b87aa22c9b0626e5f11efdd40cd9034eec42
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-21 20:50:50 +0630

    Update getVideoInfo.js

--- src/getVideoInfo.js
+++ src/getVideoInfo.js
@@ -1,9 +1,12 @@
-const { resolve } = require('path');
+const { resolve, basename } = require('path');
 const { readFileSync, writeFileSync } = require('fs');
 const { exec } = require('shelljs');
 
+const MAX_LOGGING_SIZE = 20;
 const VIDEO_DATA = resolve(__dirname, '../data/videos.json');
 const DATABASE_PATH = resolve(__dirname, '../.db');
+const GIT_AUTHOR_NAME = 'Bot';
+const GIT_AUTHOR_EMAIL = '50423290+etherio97@users.noreply.github.com';
 
 function readJSON(path) {
   let text = readFileSync(path, 'utf-8');
@@ -42,19 +45,19 @@ function updateVideo() {
     timestamp: Date.now(),
   };
   data.index = video.index + 1;
-  data.logs.push(log);
+  data.logs.unshift(log);
+  data.logs = data.logs.slice(0, MAX_LOGGING_SIZE);
   writeFileSync(DATABASE_PATH, JSON.stringify(data), "utf-8");
 }
 
 function pushChanges(message) {
   let command = [
-    "git config user.name 'skmetheloper'",
-    "git config user.email 'ethereal97@gmail.com'",
-    "git add .db",
+    "git config user.name '" + GIT_AUTHOR_NAME + "'",
+    "git config user.email '" + GIT_AUTHOR_EMAIL + "'",
+    "git add " + basename(DATABASE_PATH),
     "git commit -m 'Live Schedule'",
     "git push"
   ].join(";\n");
-
   exec(command);
   console.log('[GIT:PUSH]');
 }
