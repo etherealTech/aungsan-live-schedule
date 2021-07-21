b83154e18e633f59b2c0770f45bb722c5f10809e
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-21 14:24:41 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,64 @@
 const { default: axios } = require('axios');
+const { schedule } = require ('cron');
 const { exec } = require('shelljs');
+const { getVideo } = require('./src/getVideoInfo');
 const getFBVideo = require('./src/getFBVideo');
+const createLiveStream = require('./src/createLiveStream');
+const broadcastLiveStream = require('./src/broadcastLiveStream');
 
+const LIVE_STREAM_TITLE = 'တရား‌‌ေတာ်':
+const CRON_SCHEDULE_TIME = '0 0 19 * * *'; // every night at local time 7PM
 const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
-const postID = '654432202109918';
 
 !async function () {
-  // initiated
-  console.log(new Date().toLocaleString('en-US', {
-    timeZone: 'Asia/Yangon'
-  }, '[STARTED]'));
+  let filePath, fileName, command;
+  now('STARTED');
 
-  // auth page/group/user
+  const video = getVideo();
+  const id = video.link.split('/').pop();
+  const duration = video.length;
+
   const { data: currentAuth } = await axios.get(`https://graph.facebook.com/v10.0/me?access_token=${FACEBOOK_PAGE_TOKEN}`);
+  console.log('[AUTH]', currentAuth);
 
-  console.log('[AUTH:PAGE]', currentAuth);
+  const sources = await getFBVideo(id);
+  const { source, text } = sources.filter(source => !(source.text || '').includes('Audio')).pop();
 
-  // video details
-  const sources = await getFBVideo(postID);
-  const videos = sources.filter(source => !(source.text || '').includes('Audio'));
-  const video = videos.shift();
-  const fileName = new URL(video.source).pathname.split('/').pop();
-  const filePath = `${__dirname}/tmp/${fileName}`;
+  fileName = new URL(source).pathname.split('/').pop();
+  filePath = `${__dirname}/tmp/${fileName}`;
 
-  console.log('[INPUT]', video);
+  console.log('[INPUT]', text);
   console.log('[DOWNLOADING]', filePath);
 
-  const command = `curl -L '${video.source}' -o '${filePath}' --progress-bar `;
+  command = `curl -L '${video.source}' -o '${filePath}' --progress-bar`;
   exec(command);
 
-  console.log('[PRE-LIVE]');
-  //
+  console.log('[CRON]', CRON_SCHEDULE_TIME);
+  schedule(CRON_SCHEDULE_TIME, () => onAir(), {
+    timezone: 'Asia/Rangoon',
+  });
 
-  console.log('[LIVE]');
-  //
+  async function onAir() {
+    now('ONAIR');
 
-  console.log('DONE!');
+    const description =  (video.title || LIVE_STREAM_TITLE).split('ago')[0].slice(0, -5);
+    const { id, stream_url } = await createLiveStream({
+      title: LIVE_STREAM_TITLE,
+      description,
+      token: FACEBOOK_PAGE_TOKEN,
+    });
+
+    broadcastLiveStream(filePath, stream_url, FACEBOOK_PAGE_TOKEN);
+
+    setTimeout(() => console.log('[EXIT]') | process.exit(0), 10000);
+  }
+
 }().catch(err => console.error('[ERROR]', err));
+
+function now(message) {
+  let date = new Date();
+  let opt = {
+    timeZone: 'Asia/Yangon'
+  };
+  console.log('[' + message + ']', date.toLocaleString('en-US', opt);
+}
