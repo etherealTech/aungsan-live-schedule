a5f0885a7b5f3c91561da2b377e04cc2e4e89c76
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-27 18:17:00 +0630

    Update test.js

--- test.js
+++ test.js
@@ -2,15 +2,13 @@ const { default: axios } = require('axios');
 const { schedule } = require ('node-cron');
 const { exec } = require('shelljs');
 const { getVideo, updateVideo, pushChanges } = require('./src/getVideoInfo');
-// const getFBVideo = require('./src/getFBVideo');
 const getFBVideoFromGraph = require('./src/getFBVideoFromGraph');
 const createLiveStream = require('./src/createLiveStream');
 const broadcastLiveStream = require('./src/broadcastLiveStream');
 const generateReadme = require('./src/generateReadme');
 
 const LIVE_STREAM_TITLE = 'တရားတော်';
-const CRON_SCHEDULE_TIME = process.argv[3];
-const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
+const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_TEST_TOKEN;
 
 !async function () {
   let filePath, fileName, command;
@@ -18,20 +16,17 @@ const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
 
   const video = getVideo();
   const video_id = video.link.split('/').pop();
-  const duration = video.length;
 
   const { data: auth } = await axios.get(`https://graph.facebook.com/v10.0/me?access_token=${FACEBOOK_PAGE_TOKEN}`);
-  console.log('[AUTH]', auth);
+  console.log('[AUTH] %s (https://www.facebook.com/%s)', auth.name, auth.id);
 
   const { source, description, title } = await getFBVideoFromGraph({ id: video_id, access_token: FACEBOOK_PAGE_TOKEN });
   const text = description || title;
-  // const sources = await getFBVideo(video_id);
-  // const { source, text } = sources.filter(source => !(source.text || '').includes('Audio')).pop();
 
   fileName = new URL(source).pathname.split('/').pop();
   filePath = `${__dirname}/tmp/${fileName}`;
 
-  console.log('<<<', text);
+  console.log('<<<', source;
   console.log('>>>', filePath);
 
   command = `curl -L '${source}' -o '${filePath}' --progress-bar`;
@@ -41,10 +36,6 @@ const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
   schedule(CRON_SCHEDULE_TIME, () => onAir(), {
     timezone: 'Asia/Rangoon',
   });
-
-  generateReadme(auth, CRON_SCHEDULE_TIME);
-  updateVideo();
-  pushChanges();
   
   async function onAir() {
     now('ONAIR');
@@ -52,7 +43,7 @@ const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
       const description =  video.title || LIVE_STREAM_TITLE;
       const { id, stream_url } = await createLiveStream({
         title: LIVE_STREAM_TITLE + ' #' + video_id,
-        description,
+        description: text,
         access_token: FACEBOOK_PAGE_TOKEN,
       });
 
