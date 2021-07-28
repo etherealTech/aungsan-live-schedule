4d91b533c16b55f0a9610eb84e43fc6abe7bf3a2
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-29 01:52:31 +0630

    Update generate-readme.js

--- generate-readme.js
+++ generate-readme.js
@@ -2,7 +2,7 @@ const generateReadme = require ("./src/generateReadme");
 const { pushChanges } = require("./src/getVideoInfo");
 const { default: axios } = require("axios");
 
-const CRON_SCHEDULE_TIME = process.argv[3];
+const CRON_SCHEDULE_TIME = process.argv[3] || '0 0 19 * * *';
 const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
 
 axios.get(`https://graph.facebook.com/v10.0/me?access_token=${FACEBOOK_PAGE_TOKEN}`)
