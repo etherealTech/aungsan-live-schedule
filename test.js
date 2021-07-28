ce2ea9276d5101cf6d89c62b96f2dd8b21fe660e
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-28 22:04:57 +0630

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const { default: axios } = require('axios');
+const { schedule } = require ('node-cron');
 const { exec } = require('shelljs');
 const { getVideo, updateVideo, pushChanges } = require('./src/getVideoInfo');
 const getFBVideoFromGraph = require('./src/getFBVideoFromGraph');
@@ -7,62 +8,51 @@ const broadcastLiveStream = require('./src/broadcastLiveStream');
 const generateReadme = require('./src/generateReadme');
 
 const LIVE_STREAM_TITLE = 'တရားတော်';
+const CRON_SCHEDULE_TIME = process.argv[3];
 const FACEBOOK_PAGE_TOKEN = process.argv[2] || process.env.FACEBOOK_PAGE_TOKEN;
-const FACEBOOK_TEST_TOKEN = process.argv[3] || process.env.FACEBOOK_TEST_TOKEN;
 
-!async function () {
-  let filePath, fileName, command;
-  now('STARTED');
+!async function () { 
+  now(':INIT');
 
   const video = getVideo();
   const video_id = video.link.split('/').pop();
 
-  var { data: auth } = await axios.get(`https://graph.facebook.com/v10.0/me?access_token=${FACEBOOK_PAGE_TOKEN}`);
-  console.log('[AUTH:MAIN] %s (https://www.facebook.com/%s)', auth.name, auth.id);
-
-  var { data: auth } = await axios.get(`https://graph.facebook.com/v10.0/me?access_token=${FACEBOOK_TEST_TOKEN}`);
-  console.log('[AUTH:TEST] %s (https://www.facebook.com/%s)', auth.name, auth.id);
-
+  const { data: auth } = await axios.get(`https://graph.facebook.com/v10.0/me?access_token=${FACEBOOK_PAGE_TOKEN}`);
   const { source, description, title } = await getFBVideoFromGraph({ id: video_id, access_token: FACEBOOK_PAGE_TOKEN });
   const text = description || title;
 
-  fileName = new URL(source).pathname.split('/').pop();
-  filePath = `${__dirname}/tmp/${fileName}`;
+  const fileName = new URL(source).pathname.split('/').pop();
+  const filePath = `${__dirname}/tmp/${fileName}`;
 
-  console.log('<<<', source);
-  console.log('>>>', filePath);
+  exec(`wget '${source}' -O '${filePath}'`);
 
-  command = `curl -L '${source}' -o '${filePath}' --progress-bar`;
-  exec(command);
-  
-  setTimeout(() => onAir(), 3000);
-  
-  async function onAir() {
-    now('ONAIR');
-    try {
-      const description =  video.title || LIVE_STREAM_TITLE;
-      const { id, stream_url } = await createLiveStream({
-        title: LIVE_STREAM_TITLE + ' #' + video_id,
-        description: text,
-        access_token: FACEBOOK_TEST_TOKEN,
-      });
+  now(':PRE');
 
-      command = broadcastLiveStream(filePath, stream_url);
-      exec(command);
+  generateReadme(auth, CRON_SCHEDULE_TIME);
+  updateVideo();
+  pushChanges();
 
-      setTimeout(() => console.log('[EXIT]') | process.exit(0), 5000);
-    } catch(e) {
-      console.error(e.request.headers);
-      process.exit(1);
-    }
+  now(':LIVE');
+
+  try {
+    const { id, stream_url } = await createLiveStream({
+      title: LIVE_STREAM_TITLE + ' #' + video_id,
+      description: text,
+      access_token: FACEBOOK_PAGE_TOKEN,
+    });
+    exec(broadcastLiveStream(filePath, stream_url));
+    setTimeout(() => process.exit(0), 3000);
+  } catch(e) {
+    console.error(e.request?.headers || e.message);
+    process.exit(1);
   }
-  /* end of on air function */
-}().catch((err) => console.error('[ERROR]', err) | process.exit(1));
+}().catch((err) => {
+  console.error('[ERROR]', err);
+  process.exit(1);
+});
 
 function now(message) {
-  let date = new Date();
-  let opt = {
+  console.log('[' + message + ']', new Date().toLocaleString('en-US', {
     timeZone: 'Asia/Yangon'
-  };
-  console.log('[' + message + ']', date.toLocaleString('en-US', opt));
+  }));
 }
